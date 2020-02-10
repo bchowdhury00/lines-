0d487dbaea10ca9accfdea71a7b761c83800fac4
Biraj Chowdhury <bchowdhury00@stuy.edu>
2020-02-10 11:44:51 -0500

    worked on sieve

--- draw.c
+++ draw.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 #include "ml6.h"
 #include "display.h"
 #include "draw.h"
-
 //Insert your line algorithm here
 void draw_line(int x0, int y0, int x1, int y1, screen s, color c) {
     if (x1 == x0){
@@ -120,4 +120,27 @@ void octantFourLine(int x0, int y0, int x1, int y1, screen s, color c){
         D += 2 * A;
         x += 1;
     }
+}
+
+void findMidpoint(int x0,int y0, int x1, int y1, int * x, int * y){
+  double x2 = ((double)x1 - (double)x0)/2;
+  double y2 = ((double)y1 - (double)y0)/2;
+  *x = floor(x2);
+  *y = floor(y2);
+  return;
+}
+
+void sierpinski_sieve(screen s, color c, int iterations, int x0, int y0,int x1,int x2,int x3){
+  if (iterations <= 0){
+    return;
+  }
+  int * newx0, newx1, newx2, newy0,newy1,newy2;
+  findMidpoint(x0,y0,x1,y1,newx0,newy0);
+  findMidpoint(x0,y0,x2,y2,newx1,newy1);
+  findMidpoint(x1,y1,x2,y2,newx2,newy2);
+  draw_line(newx0,newy0,newx1,newy1,s,c);
+  draw_line(newx1,newy1,newx2,newy2,s,c);
+  draw_line(newx0,newy0,newx2,newy2,s,c);
+
+
 }
